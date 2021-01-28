3e57ebb8e39d7116a81e7eb044670a76d730055c
Nikita <69696820+0Nikita01@users.noreply.github.com>
2021-01-28 03:40:46 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+/* import HeaderBlock from "./components/headerBlocks"; */
+import Header from './components/Header';
+import Layout from './components/Layout';
+import bg2 from './assets/bg2.jpg';
+import bg3 from './assets/bg3.jpg';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+	return (
+		<>
+			<Header title="This is title" descr="This is Description!" />
+			<Layout 
+				id="first" 
+				title="This is title" 
+				descr="This is Description!" 
+				urlBg={bg2} 
+			/>
+			<Layout 
+				id="second" 
+				title="This is title" 
+				descr="This is Description!" 
+				colorBg="#000000" 
+			/>
+			<Layout 
+				id="third" 
+				title="This is title" 
+				descr="This is Description!" 
+				urlBg={bg3} 
+			/>
+			<Footer />
+		</>
+	)
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,36 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./App";
 import './index.css';
-import HeaderBlock from "./components/headerBlocks";
 
-const AppList = () => {
-	const items = ['Item1', 'Item2'];
-	return (
-		<>
-			<ul>
-				{
-					items.map(item => <li>{item}</li>)
-				}
-				<li>{ items[0]}</li>
-				<li>{ items[1]}</li>
-			</ul>
-		</>
-	)
-}
-
-const AppHeader = () => {
-	return <h1 className="header">Hello World!</h1>
-
-}
-
-const App = () => {
-	return (
-		<>
-			<HeaderBlock/>
-			<AppHeader/>
-			<AppList/>
-		</>
-	)
-}
 
 ReactDOM.render(<App/>, document.getElementById('root'));
