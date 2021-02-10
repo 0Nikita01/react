9aee0308addd6566e5830e6c2323f53c49236400
Nikita <69696820+0Nikita01@users.noreply.github.com>
2021-02-11 02:06:43 +0300

    homework-7

--- src/components/PockemonCard/index.js
+++ src/components/PockemonCard/index.js
@@ -1,8 +1,7 @@
-import CardStyle from './style.module.css';
-import Back from './assets/card-back-side.jpg';
+import s from './style.module.css';
 import cn from 'classnames';
 
-const PockemonCard = ({name, img, id, type, values, isActive, cardClick}) => {
+const PockemonCard = ({className, name, img, id, type, values, minimize, isActive, cardClick, isSelected}) => {
 
     /*
     const [isActive, setActive] = useState(false);
@@ -15,36 +14,40 @@ const PockemonCard = ({name, img, id, type, values, isActive, cardClick}) => {
     }
 
     return (
-        <div className={CardStyle.root} onClick={handleClick}>
-            <div className={cn(CardStyle.pokemonCard, {[CardStyle.active]: isActive})}>
-                <div className={CardStyle.cardFront}>
-                    <div className={cn(CardStyle.wrap , CardStyle.front)}>
-                        <div className={`${CardStyle.pokemon} ${CardStyle[type]}`}>
-                            <div className={CardStyle.values}>
-                                <div className={cn(CardStyle.count ,CardStyle.top)}>{values.top}</div>
-                                <div className={cn(CardStyle.count ,CardStyle.right)}>{values.right}</div>
-                                <div className={cn(CardStyle.count ,CardStyle.bottom)}>{values.bottom}</div>
-                                <div className={cn(CardStyle.count ,CardStyle.left)}>{values.left}</div>
-                            </div>
-                            <div className={CardStyle.imgContainer}>
-                                <img src={img} alt={name} />
-                            </div>
-                            <div className={CardStyle.info}>
-                                <span className={CardStyle.number}>#{id}</span>
-                                <h3 className={CardStyle.name}>{name}</h3>
-                                <small className={CardStyle.type}>Type: <span>{type}</span></small>
-                            </div>
+        <div 
+            className={cn(className, s.pokemonCard, {
+                [s.active]: isActive,
+                [s.selected]: isSelected,
+            })} onClick={handleClick}>
+            <div className={s.cardFront}>
+                <div className={cn(s.wrap, s.front)}>
+                    <div className={cn(s.pokemon, s[type])}>
+                        <div className={s.values}>
+                            <div className={cn(s.count, s.top)}>{values.top}</div>
+                            <div className={cn(s.count, s.right)}>{values.right}</div>
+                            <div className={cn(s.count, s.bottom)}>{values.bottom}</div>
+                            <div className={cn(s.count, s.left)}>{values.left}</div>
                         </div>
+                        <div className={s.imgContainer}>
+                            <img src={img} alt={name} />
+                        </div>
+                        { !minimize && (<div className={s.info}>
+                            <span className={s.number}>#{id}</span>
+                            <h3 className={s.name}>
+                                {name}
+                            </h3>
+                            <small className={s.type}>
+                                Type: <span>{type}</span>
+                            </small>
+                        </div>) }
                     </div>
                 </div>
-
-                <div className={CardStyle.cardBack}>
-                    <div className={cn(CardStyle.wrap, CardStyle.back)}>
-                        <img src={Back} alt="Сard Backed" />
-                    </div>
-                </div>
-
             </div>
+        
+            <div className={s.cardBack}>
+                <div className={cn(s.wrap, s.back)} />
+            </div>
+        
         </div>
     )
 }
--- src/routes/Game/index.js
+++ src/routes/Game/index.js
@@ -1,100 +1,40 @@
-import { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import PockemonCard from '../../components/PockemonCard';
+import { useState } from 'react';
+import {useRouteMatch, Switch, Route} from 'react-router-dom';
+import { PokemonContext } from '../../context/pokemonContext';
+import BoardPage from './routes/Board';
+import FinishPage from './routes/Finish';
+import StartPage from './routes/Start';
 
-import database from '../../service/firebase';
-import cards from '../../json/cards.json';
-
-import s from './style.module.css';
-
-const POCKEMONS = cards;
-const GamePage = ({onChangePage}) => {
-    const [Pockemons, setPockemons] = useState({});
-
-    useEffect(() => {
-        database.ref('pokemons').once('value', (snapshot) => {
-            setPockemons(snapshot.val());
-        })
-    }, []);
-
-    const handleCardClick = (id) => {
-        setPockemons(prevState => {
-            return Object.entries(prevState).reduce((acc, item) => {
-                const pokemon = {...item[1]};
-                if (pokemon.id === id) {
-                    pokemon.active = !pokemon.active;
-                    
-                    database.ref('pokemons/' + item[0]).set(pokemon);
-                };
+const GamePage = () => {
+    const [selectedPokemons, setSelectedPokemons] = useState({});
+    const match = useRouteMatch();
+    const handlerSelectedPokemons = (key, pokemon) => {
         
-                acc[item[0]] = pokemon;
-        
-                return acc;
-            }, {});
-        });
-        
-        /*
-        setPockemons(prevState => prevState.map(item => {
-            const newItem = {...item}
-            if (newItem["id"] === id){
-                newItem["active"] = !newItem["active"];
+        setSelectedPokemons(prevState => {
+            if (prevState[key])
+            {
+                const copyState = {...prevState};
+                delete copyState[key];
+
+                return copyState;
             }
-            return newItem;
-        }));*/
-    };
-
-    const randomID = (min, max, district = false) => {
-        let rand = Math.round(min - 0.5 + Math.random() * (max - min + 1));
-
-        if (district)
-        {
-            Object.entries(Pockemons).forEach(item => {
-                if (rand === item[1].id)
-                {
-                    rand = randomID(min, max, district);
-                }
-            });
-        }
-        
-        return rand;
-    }
-
-    const AddNewPokemon = () => {
-        const newKey = database.ref().child('pokemons').push().key;
-        const id = randomID(1, 100, true);
-        const index = randomID(0, 4);
-        const newPokemon = POCKEMONS[index];
-        const newPockemons = Object.fromEntries(Object.entries(Pockemons).map(item => {return item}));
-        newPokemon.id = id;
-        database.ref('pokemons/' + newKey)
-            .set(newPokemon)
-            .then(() => {
-                newPockemons[newKey] = newPokemon;
-                setPockemons(newPockemons);
-            });      
-    }
-
-    const history = useHistory();
-
-    const handlerClick = () => {
-        history.push('/home');
+            return {
+                ...prevState,
+                [key]: pokemon,
+            }
+        })
     }
     return (
-        <>
-            <div className={s.button}>
-                <button onClick={AddNewPokemon}>ADD NEW POKEMON</button>
-            </div>
-            <div className={s.flex}>
-				{
-					Object.entries(Pockemons).map(([key, {name, img, id, type, values, active}]) => <PockemonCard key={id} name={name} img={img} id={id} type={type} values={values} isActive={active} cardClick={handleCardClick}/>)
-				}
-			</div>
-            <div>
-                <button onClick={handlerClick}>
-                    Home
-                </button>
-            </div>
-        </>
+        <PokemonContext.Provider value={{
+            pokemon: selectedPokemons,
+            onSelectedPokemons: handlerSelectedPokemons
+        }}>
+            <Switch>
+                <Route path={`${match.path}/`} exact component={StartPage} />
+                <Route path={`${match.path}/board`} component={BoardPage} />
+                <Route path={`${match.path}/finish`} component={FinishPage} />
+            </Switch>
+        </PokemonContext.Provider>
     );
 };
 
